b35c718f4bdee520c09ff0a974ab724afe14dfa9
lwilliams002 <cmario72.lw@gmail.com>
2025-05-08 12:58:16 -0700

    function sendMail(){     // Gather form values     const parms = {         from_name: document.querySelector("#name").value,         reply_to:  document.querySelector("#email").value,         subject:   document.querySelector("#subject").value,         message:   document.querySelector("#message").value,         to_email:  "gomezqpainting@gmail.com"     };
        emailjs.send('service_txrdsd4', 'template_qlgwp1c', parms)
          .then(() => {
            alert("Email has been sent!!");
            window.location.reload();
          })
          .catch((err) => {
            console.error(err);
            alert("Failed to send email: " + err.text);
          });
    }

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function sendMail(){
+    // Gather form values
     const parms = {
         name: document.querySelector("#name").value,
-        email: document.querySelector("#email").value,
-        phone: document.querySelector("#subject").value,
-        message: document.querySelector("#message").value,
+        email:  document.querySelector("#email").value,
+        phone:   document.querySelector("#subject").value,
+        message:   document.querySelector("#message").value,
     };
 
-    emailjs
-        .send('service_txrdsd4', 'template_qlgwp1c', parms)
-        .then(()=>{
-            alert("Email has been sent!!");
-    })
-        .catch((err)=>{
-            console.log(err);
-            alert(err);
-        })
+    emailjs.send('service_gyuwhda', 'template_upy29dc', parms)
+      .then(() => {
+        alert("Email has been sent!!");
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Failed to send email: " + err.text);
+      });
 }
